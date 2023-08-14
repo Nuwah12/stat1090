f49e66d01cc127b974944a23b76ee32c61bbf3ea
Nuwah12 <ngb23@pitt.edu>
2023-08-13 21:25:34 -0400

    verify data capture from dump190 webserver

--- poll/poll_1090.sh
+++ poll/poll_1090.sh
@@ -2,4 +2,20 @@
 
 ##########
 # Poll the dump1090 webserver for aircraft json data
-# 
+# run ./dump1090 --net 
+##########
+
+if [[ -z $1 ]]; then
+    echo "Pass in localhost and port"
+    exit 1
+fi
+
+#TMP="./tmp"
+#mkdir -p ${TMP}
+
+while true; do
+    wget --no-parent -A json -nH -np "${1}/data.json"
+    cat data.json
+    sleep 10
+    rm data.json
+done
